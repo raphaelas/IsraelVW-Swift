ff421e1f1f30f5898c2d2ea952b93964e59a7584
Raphael Astrow <rastrow@andrew.cmu.edu>
2015-01-12 15:04:40 -0500

    Initial Commit

---
+++ IsraelVirtualWater/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  IsraelVirtualWater
+//
+//  Created by Raphael Astrow on 1/12/15.
+//  Copyright (c) 2015 Raphael Astrow. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
